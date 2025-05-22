cbd07c38cd6381ad6b72fbad8a722eb8bf718504
Gopinath2308 <gopinathsamanta2308@gmail.com>
2025-05-22 12:50:04 +0530

    Ft Commit

--- portfolio.js
+++ portfolio.js
@@ -6,6 +6,28 @@ var typed = new Typed(".text",{
     loop: true
 });
 
+let sections=document.querySelectorAll('section');
+let navlinks=document.querySelectorAll('header nav a');
+window.onscroll=()=>{
+    sections.forEach(sec =>{
+        let top=window.scrollY;
+        console.log("top is"+top);
+        let offset=sec.offsetTop-100;
+        console.log("offset is"+offset);
+        let height=sec.offsetHeight;
+        console.log("height is"+height);
+        let id=sec.getAttribute('id')
+        console.log(id);
+        if(top>=offset && top< offset+height)
+        {
+            navlinks.forEach(links=>{
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*='+id+']').classList.add('active');
+            })
+        }
+    });
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     const menuIcon = document.getElementById("manu-icon");
     const menu = document.querySelector(".menu");
